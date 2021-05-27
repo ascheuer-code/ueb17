3bc7be30378492037867bc2a3b644b764fe1270b
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-05-27 08:22:31 +0200

    fertig

--- src/Fac.java
+++ src/Fac.java
@@ -1,8 +1,8 @@
-public class Fac implements MyFunction{
+public class Fac implements MyFunction {
 
     @Override
     public int apply(int i) {
         return (i == 0) ? 1 : i * apply(i - 1);
     }
-    
+
 }
--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,17 @@ public class Main {
     private final int min = 0;
     private final int max = 10;
 
+    // Definierte Lambda Funktionen
+
     private final MyFunction sqrL = i -> i * i;
     private final MyFunction facL = i -> (i == 0) ? 1 : i * this.facL.apply(i - 1);
     private final MyFunction powL = i -> (int) Math.pow(i, i + 1);
     private final MyFunction fibL = i -> (i <= 2) ? 1 : this.fibL.apply(i - 1) + this.fibL.apply(i - 2);
     private final Preadikat even = i -> i % 2 != 0;
+
+    /**
+     * Annonyme Klasse fuer x^2
+     */
 
     MyFunction sqrA = new MyFunction() {
         @Override
@@ -22,12 +28,20 @@ public class Main {
         }
     };
 
+    /**
+     * Annonyme Klasse fuer fakultaet
+     */
+
     MyFunction facA = new MyFunction() {
         @Override
         public int apply(int i) {
             return (i == 0) ? 1 : i * apply(i - 1);
         }
     };
+
+    /**
+     * Annonyme Klasse fuer hoch x +1
+     */
 
     MyFunction powA = new MyFunction() {
         @Override
@@ -36,6 +50,10 @@ public class Main {
         }
     };
 
+    /**
+     * Annonyme Klasse fuer fibonacci zahlenreihe
+     */
+
     MyFunction fibA = new MyFunction() {
         @Override
         public int apply(int i) {
@@ -43,12 +61,20 @@ public class Main {
         }
     };
 
+    /**
+     * Annonyme Klasse zur pruefung ob ein Wert gerade ist
+     */
+
     Preadikat odd = new Preadikat() {
         @Override
         public boolean checkNumber(int i) {
             return i % 2 == 0;
         }
     };
+
+    /**
+     * Static nested class von fakultaet
+     */
 
     public static class FacStatic implements MyFunction {
 
@@ -58,6 +84,10 @@ public class Main {
         }
 
     }
+
+    /**
+     * Start Methode
+     */
 
     public void start() {
         // f(x) = x*x
@@ -107,11 +137,19 @@ public class Main {
         System.out.println("--------------------------------------------");
     }
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
 
         new Main().start();
     }
 
+    /**
+     * @param i   darf nicht kleiner 0 sein
+     * @param j   darf nicht kleiner 0 sein
+     * @param fkt übergebene Funktion die ausgefuehrt werden soll
+     */
     public static void applyAndPrint(int i, int j, MyFunction fkt) {
         Lib_Digits.checkMin(0, i, "Min");
         Lib_Digits.checkMin(0, j, "Max");
